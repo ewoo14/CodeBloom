5c721ee9c3142227a32639f452a1ab840c19c997
CLovesJava <wntjdcl1@gmail.com>
2024-11-08 01:44:50 +0900

    [Fix] BaseEntity 컬럼 수정

--- src/main/java/com/sparta/project/domain/BaseEntity.java
+++ src/main/java/com/sparta/project/domain/BaseEntity.java
@@ -17,10 +17,14 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 <<<<<<< HEAD
+<<<<<<< HEAD
 public abstract class BaseEntity {
 =======
 public class BaseEntity {
 >>>>>>> 6ddac12 ([Fix] createdAt 컬럼 옵션에 "nullable=true" 제거)
+=======
+public abstract class BaseEntity {
+>>>>>>> f6a68d0 ([Fix] BaseEntity 컬럼 수정)
     @Column(name="is_deleted") // 삭제 여부
     @ColumnDefault("false")
     private Boolean isDeleted;
@@ -36,6 +40,7 @@ public class BaseEntity {
     @Column(name="deleted_at")
     private LocalDateTime deletedAt;
 
+<<<<<<< HEAD
     @CreatedBy
     @Column(name="created_by", length=2048)
     private String createdBy;
@@ -48,6 +53,25 @@ public class BaseEntity {
     private String deletedBy;
 
     public void deleteBase(String username) {
+=======
+//    @CreatedBy
+    @Column(name="created_by", length=10, updatable = false)
+    private String createdBy;
+
+//    @LastModifiedBy
+    @Column(name="updated_by", length=10)
+    private String updatedBy;
+
+    @Column(name="deleted_by", length=10)
+    private String deletedBy;
+
+    /*
+    createdBy와 updatedBy는 스프링 시큐리티 부분이 구현되면 AuditorAware 구현체를 사용하는 방법으로 구현하겠습니다.
+    참고: https://javacpro.tistory.com/85
+     */
+
+    public void delete(String username) {
+>>>>>>> f6a68d0 ([Fix] BaseEntity 컬럼 수정)
         this.isDeleted = true;
         this.deletedAt = LocalDateTime.now();
         this.deletedBy = username;
