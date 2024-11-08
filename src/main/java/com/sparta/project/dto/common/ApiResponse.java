4ebd14e54ec17d88edc6d5dbd1a91bc54cbdf8aa
siwon <muncool39@gmail.com>
2024-11-09 03:39:04 +0900

    [Feat] 공통 응답 객체 추가

--- src/main/java/com/sparta/project/dto/common/ApiResponse.java
+++ src/main/java/com/sparta/project/dto/common/ApiResponse.java
@@ -7,11 +7,20 @@ public record ApiResponse<T> (
         String message,
         T data
 ) {
+<<<<<<< HEAD
     public static <T> ApiResponse<T> success(T data) {
         return new ApiResponse<>("API 요청에 성공했습니다", data);
     }
 
     public static ApiResponse<Void> success() {
         return new ApiResponse<>("API 요청에 성공했습니다", null);
+=======
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(message, data);
+    }
+
+    public static ApiResponse<Void> success(String message) {
+        return new ApiResponse<>(message, null);
+>>>>>>> cb01438 ([Feat] 공통 응답 객체 추가)
     }
 }
