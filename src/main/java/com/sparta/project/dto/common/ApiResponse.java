eeaf0173d8de5fe8f437471763352383786f0441
siwon <muncool39@gmail.com>
2024-11-09 14:38:11 +0900

    [Fix] API 요청 성공시의 공통 메세지가 나가도록 변경

--- src/main/java/com/sparta/project/dto/common/ApiResponse.java
+++ src/main/java/com/sparta/project/dto/common/ApiResponse.java
@@ -8,6 +8,7 @@ public record ApiResponse<T> (
         T data
 ) {
 <<<<<<< HEAD
+<<<<<<< HEAD
     public static <T> ApiResponse<T> success(T data) {
         return new ApiResponse<>("API 요청에 성공했습니다", data);
     }
@@ -22,5 +23,13 @@ public record ApiResponse<T> (
     public static ApiResponse<Void> success(String message) {
         return new ApiResponse<>(message, null);
 >>>>>>> cb01438 ([Feat] 공통 응답 객체 추가)
+=======
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>("API 요청에 성공했습니다", data);
+    }
+
+    public static ApiResponse<Void> success() {
+        return new ApiResponse<>("API 요청에 성공했습니다", null);
+>>>>>>> daa7309 ([Fix] API 요청 성공시의 공통 메세지가 나가도록 변경)
     }
 }
