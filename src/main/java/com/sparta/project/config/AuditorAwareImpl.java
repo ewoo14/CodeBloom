f41ef95a72064c956084d9b5cf81792d7ff6d806
Griotold <gotjd9773@naver.com>
2024-11-11 12:01:37 +0900

    [Fix] AuditorAwareImpl<String> -> AuditorAwareImpl<Long> 으로 수정

--- src/main/java/com/sparta/project/config/AuditorAwareImpl.java
+++ src/main/java/com/sparta/project/config/AuditorAwareImpl.java
@@ -1,10 +1,13 @@
 package com.sparta.project.config;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import lombok.extern.slf4j.Slf4j;
 =======
 import com.sparta.project.config.jwt.UserDetail;
 >>>>>>> 279b3b6 (AuditorAware 적용)
+=======
+>>>>>>> 980f191 ([Fix] AuditorAwareImpl<String> -> AuditorAwareImpl<Long> 으로 수정)
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,13 +15,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import java.util.Optional;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 @Slf4j
 =======
 >>>>>>> 279b3b6 (AuditorAware 적용)
 public class AuditorAwareImpl implements AuditorAware<String> {
+=======
+public class AuditorAwareImpl implements AuditorAware<Long> {
+>>>>>>> 980f191 ([Fix] AuditorAwareImpl<String> -> AuditorAwareImpl<Long> 으로 수정)
 
     @Override
-    public Optional<String> getCurrentAuditor() {
+    public Optional<Long> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
 <<<<<<< HEAD
@@ -31,6 +38,7 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 
         //사용자 환경에 맞게 로그인한 사용자의 정보를 불러온다.
 <<<<<<< HEAD
+<<<<<<< HEAD
         log.info("authentication.getName(): {}", authentication.getName());
         if (authentication.getName().equals("anonymousUser")) {
             return Optional.of("anonymous");
@@ -42,6 +50,10 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 
         return Optional.of(userDetail.userId());
 >>>>>>> 279b3b6 (AuditorAware 적용)
+=======
+
+        return Optional.of(Long.parseLong(authentication.getName()));
+>>>>>>> 980f191 ([Fix] AuditorAwareImpl<String> -> AuditorAwareImpl<Long> 으로 수정)
     }
 
 }
