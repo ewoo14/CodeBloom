6d2475f0d1eaee5ce2b48a153425287df58c1bf3
siwon <muncool39@gmail.com>
2024-11-12 15:19:18 +0900

    [Feat] 배송지 업데이트 기능

--- src/main/java/com/sparta/project/service/AddressService.java
+++ src/main/java/com/sparta/project/service/AddressService.java
@@ -5,6 +5,7 @@ import com.sparta.project.domain.Address;
 import com.sparta.project.domain.User;
 import com.sparta.project.domain.enums.Role;
 import com.sparta.project.dto.address.AddressCreateRequest;
+import com.sparta.project.dto.address.AddressUpdateRequest;
 import com.sparta.project.exception.CodeBloomException;
 import com.sparta.project.exception.ErrorCode;
 import com.sparta.project.repository.AddressRepository;
@@ -39,12 +40,28 @@ public class AddressService {
         ));
     }
 
+    @Transactional
+    public void updateAddress(long userId, String addressId, AddressUpdateRequest request) {
+        User user = userService.getUserOrException(userId);
+        Address address = getAddressOrException(addressId);
+        address.update(request.city(), request.district(), request.streetName(),
+                request.streetNumber(), request.detail(), request.isDefault()
+        );
+    }
+
     private boolean overMaxAddress(final User user) {
         return addressRepository.countByUser(user) > 5;
     }
 
     private boolean alreadyExistDefault(final User user) {
         return addressRepository.existsByUserAndIsDefault(user, true);
+    }
+
+
+    private Address getAddressOrException(final String addressId) {
+        return addressRepository.findById(addressId).orElseThrow(()->
+                new CodeBloomException(ErrorCode.ADDRESS_NOT_FOUND)
+        );
     }
 
 }
