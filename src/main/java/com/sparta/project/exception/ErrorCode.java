00a7e0dbc2090dace8cd0efbd6b938f76a8bd796
siwon <muncool39@gmail.com>
2024-11-12 15:17:34 +0900

    [Feat] 일치하는 배송지 정보가 없는 경우의 에러 코드 추가

--- src/main/java/com/sparta/project/exception/ErrorCode.java
+++ src/main/java/com/sparta/project/exception/ErrorCode.java
@@ -19,6 +19,7 @@ public enum ErrorCode {
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호 정보가 일치하지 않습니다."),
 
     // Address
+    ADDRESS_NOT_FOUND(HttpStatus.NOT_FOUND, "일치하는 배송지 정보가 존재하지 않습니다."),
     EXCEED_MAXIMUM_ADDRESS(HttpStatus.CONFLICT, "등록할 수 있는 최대 배송지 수를 초과했습니다."),
 
     // Location
