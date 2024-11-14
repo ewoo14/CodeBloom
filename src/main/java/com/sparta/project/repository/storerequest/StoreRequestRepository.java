66725a10853a5dd13dde1d3690e14da8568c19f7
siwon <muncool39@gmail.com>
2024-11-14 20:05:51 +0900

    [Feat] 음식점 생성 요청 목록 조회 API

--- src/main/java/com/sparta/project/repository/storerequest/StoreRequestRepository.java
+++ src/main/java/com/sparta/project/repository/storerequest/StoreRequestRepository.java
@@ -3,5 +3,5 @@ package com.sparta.project.repository.storerequest;
 import com.sparta.project.domain.StoreRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StoreRequestRepository extends JpaRepository<StoreRequest, String> {
+public interface StoreRequestRepository extends JpaRepository<StoreRequest, String>, StoreRequestCustomRepository {
 }
