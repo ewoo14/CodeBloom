299386af4e59777820ee7f481dcb524967023d65
siwon <muncool39@gmail.com>
2024-11-15 00:07:07 +0900

    [Feat] 음식점 생성 요청 상세 조회 API

--- src/main/java/com/sparta/project/service/StoreCategoryService.java
+++ src/main/java/com/sparta/project/service/StoreCategoryService.java
@@ -1,8 +1,13 @@
 package com.sparta.project.service;
 
 import com.sparta.project.domain.StoreCategory;
+import com.sparta.project.domain.User;
+import com.sparta.project.domain.enums.Role;
+import com.sparta.project.dto.storecategory.StoreCategoryAdminResponse;
 import com.sparta.project.dto.storecategory.StoreCategoryCreateRequest;
+import com.sparta.project.dto.storecategory.StoreCategoryResponse;
 import com.sparta.project.dto.storecategory.StoreCategoryUpdateRequest;
+import com.sparta.project.dto.storecategory.StoreCategoryUserResponse;
 import com.sparta.project.exception.CodeBloomException;
 import com.sparta.project.exception.ErrorCode;
 import com.sparta.project.repository.StoreCategoryRepository;
@@ -14,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class StoreCategoryService {
 
+    private final UserService userService;
     private final StoreCategoryRepository storeCategoryRepository;
 
     public StoreCategory getStoreCategoryOrException(String categoryId) {
@@ -38,6 +44,22 @@ public class StoreCategoryService {
     public void deleteStoreCategory(String userId, String categoryId) {
         StoreCategory storeCategory = getStoreCategoryOrException(categoryId);
         storeCategory.deleteBase(userId);
+    }
+
+    @Transactional(readOnly = true)
+    public StoreCategoryResponse getCategoryById(long userId, String categoryId) {
+        StoreCategory storeCategory = getStoreCategoryOrException(categoryId);
+        User user = userService.getUserOrException(userId);
+        if(!isAdmin(user.getRole()) && storeCategory.getIsDeleted()!=null) {
+            throw new CodeBloomException(ErrorCode.FORBIDDEN_ACCESS);
+        }
+        return (isAdmin(user.getRole()))
+                ? StoreCategoryAdminResponse.from(storeCategory)
+                : StoreCategoryUserResponse.from(storeCategory);
+    }
+
+    private boolean isAdmin(Role role) {
+        return role == Role.MANAGER || role == Role.MASTER;
     }
 
     private void checkNameDuplication(String newName) {
