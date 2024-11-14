9aca73fa41a59f511a025c5c52b95cd9853bb678
Griotold <gotjd9773@naver.com>
2024-11-14 15:55:36 +0900

    [Fix] 주문상세조회 피드백 반영
    1. userId 빼기
    2. deletedAt -> rejectedAt

--- src/main/java/com/sparta/project/dto/order/OrderResponse.java
+++ src/main/java/com/sparta/project/dto/order/OrderResponse.java
@@ -6,27 +6,37 @@ import java.time.LocalDateTime;
 
 public record OrderResponse(
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
         Long userId,
 >>>>>>> 23b4e98 ([Feat] 주문 상세 조회)
+=======
+>>>>>>> 1d93c8f ([Fix] 주문상세조회 피드백 반영)
         String orderId,
         String type,
         String status,
         int orderPrice,
         LocalDateTime createdAt,
 <<<<<<< HEAD
+<<<<<<< HEAD
         LocalDateTime rejectedAt
 =======
         LocalDateTime deletedAt
 >>>>>>> 23b4e98 ([Feat] 주문 상세 조회)
+=======
+        LocalDateTime rejectedAt
+>>>>>>> 1d93c8f ([Fix] 주문상세조회 피드백 반영)
 ) {
 
     public static OrderResponse from(Order order) {
         return new OrderResponse(
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
                 order.getUser().getUserId(),
 >>>>>>> 23b4e98 ([Feat] 주문 상세 조회)
+=======
+>>>>>>> 1d93c8f ([Fix] 주문상세조회 피드백 반영)
                 order.getOrderId(),
                 order.getType().name(),
                 order.getStatus().name(),
