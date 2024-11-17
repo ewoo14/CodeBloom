3a548377a27d40ede438e303974e3ff71ee326cf
Griotold <gotjd9773@naver.com>
2024-11-17 10:01:39 +0900

    [Fix] 평점을 계산할 때 소수점 두 번째 자리에서 반올림하여 "4.33333..." -> "4.3" 형태로 표현되도록 수정

--- src/main/java/com/sparta/project/repository/ReviewRepository.java
+++ src/main/java/com/sparta/project/repository/ReviewRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, String>, QuerydslPredicateExecutor<Review> {
     @Query("SELECT ROUND(AVG(CAST(r.score AS double)), 1) FROM Review r WHERE r.store.storeId = :storeId AND r.isDeleted = false")
+<<<<<<< HEAD
     Double calculateAverageScoreByStoreId(@Param("storeId") String storeId);
 =======
 import org.springframework.data.domain.Page;
@@ -26,6 +27,8 @@ public interface ReviewRepository extends JpaRepository<Review, String>, Queryds
 >>>>>>> 0ebca46 ([Feat] 리뷰 생성 및 조회 구현)
 =======
     @Query("SELECT AVG(CAST(r.score AS double)) FROM Review r WHERE r.store.storeId = :storeId AND r.isDeleted = false")
+=======
+>>>>>>> abb6662 ([Fix] 평점을 계산할 때 소수점 두 번째 자리에서 반올림하여 "4.33333..." -> "4.3" 형태로 표현되도록 수정)
     Double calculateAverageScoreByStoreId(@Param("storeId") String storeId);
 >>>>>>> 4ed121a ([Feat] 평점 업데이트 - 1시간마다 스케쥴러 동작)
 }
